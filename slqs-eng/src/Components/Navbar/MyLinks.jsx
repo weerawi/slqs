17e176f7bbc72a34be38c7e7af593274353375be
Weerawi <weerawikramatharindu@gmail.com>
2024-03-25 11:22:47 +0530

    navabar link list created

---
+++ slqs-eng/src/Components/Navbar/MyLinks.jsx
@@ -0,0 +1,31 @@
+export const links = [
+    {
+      name: "Members",
+      submenu: true,
+      sublinks: [
+        
+            { name: "Commitee", link: "/commitee" },
+            { name: "Index", link: "/index" },
+            { name: "Training & Events", link: "/training" },
+            { name: "Jobs in Kuwait", link: "/jobs" },
+            { name: "Admin", link: "/admin" },
+         
+          
+      ],
+    },
+    
+    {
+      name: "Blog & Media",
+      submenu: true,
+      sublinks: [
+        
+        
+            { name: "Article", link: "/article" },
+            { name: "CSR", link: "/csr" },
+            { name: "Gallery", link: "/gallery" }, 
+       
+         
+      ],
+    },
+     
+  ];
