f6326c268067c96edabc01f07fe860f756accacd
Weerawi <weerawikramatharindu@gmail.com>
2024-05-28 14:37:59 +0530

    create gallery page functinal completely

--- slqs-eng/src/Components/GalleryCard.jsx
+++ slqs-eng/src/Components/GalleryCard.jsx
@@ -1,18 +1,27 @@
-import React from 'react' 
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const GalleryCard = (props) => {
-  return (
-    <div>
-        
-        
-              <Link to={`/gallery/${props.id}`} className=" "   >
-                <img className="w-60 h-100" src={props.image}  alt="Image" />
-              </Link>
-           
+  const [isHovered, setIsHovered] = useState(false);
 
+  return (
+    <div className='hover:scale-105 transition-all duration-500 pb-10 '>
+      <div 
+        className={`w-56 h-56 sm:w-72 sm:h-72 md:w-80 md:h-80 transition-shadow duration-500 rounded-xl   ${isHovered ? 'shadow-lg' : ''}`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={{
+          boxShadow: isHovered 
+            ? 'rgba(130, 0, 0, 0.4) 5px 5px, rgba(130, 0, 0, 0.3) 10px 10px, rgba(130, 0, 0, 0.2) 15px 15px, rgba(130, 0, 0, 0.1) 20px 20px, rgba(130, 0, 0, 0.05) 25px 25px' 
+            : 'rgba(130, 0, 0, 0.8) 5px 5px 10px'
+        }}
+      >
+        <Link to={`/gallery/${props.id}`}>
+          <img className="w-full h-full rounded-xl" src={props.image} alt="Image" />
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default GalleryCard
+export default GalleryCard;
--- slqs-eng/src/Pages/Gallery.jsx
+++ slqs-eng/src/Pages/Gallery.jsx
@@ -14,7 +14,7 @@ const Gallery = () => {
         <PageTitle name="gallery" image={galleryimg}/>
 
 
-        <div className='flex items-center justify-center max-w-[1500px] mx-auto'>
+        <div className='flex items-center justify-center max-w-[1500px] mx-auto py-10'>
           <div className='grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 py-10  '>
 
                 {gallery_data.map((item,i)=>{
