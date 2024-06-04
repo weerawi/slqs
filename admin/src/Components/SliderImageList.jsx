179859b9fe1515d60ee4e227f988574b6f105c02
Weerawi <weerawikramatharindu@gmail.com>
2024-06-04 18:56:55 +0530

    create slider image adding and viewing component admin panel

---
+++ admin/src/Components/SliderImageList.jsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import remove_icon from '../assets/cross_icon.png';
+
+ 
+
+const SliderImageList = () => {
+
+
+    const [sliderImageData, setSliderImageData] = useState([]);
+
+    const fetchInfo = async() => {
+        await fetch('http://localhost:4000/allsliderimages')
+        .then((res)=>res.json())
+        .then((data)=>setSliderImageData(data))
+    }
+
+    useEffect(() => {
+        fetchInfo();
+    },[])
+
+
+    const remove_product = async(id) => {
+        await fetch(`http://localhost:4000/removesliderimage`,{
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+                'content-Type':'application/json'
+            },
+            body:JSON.stringify({id:id}),
+        })
+        await fetchInfo();
+        
+    }
+
+  return (
+    <div className="flex flex-col items-center w-[80%] mx-auto"> 
+
+        <h1 className="text-3xl font-bold p-5">All Slider Images</h1>
+        <div className=" font-semibold  items-center grid grid-cols-5 gap-1 md:gap-2 lg:gap-10 py-1 justify-around ">
+            {/* <p className='flex justify-center'>Products</p>
+            <p className='col-start-2 col-span-3 flex justify-center '>Title</p> */}
+            <p className='flex justify-center' >Image</p> 
+            <p className='flex justify-center' >Title</p> 
+            <p className='flex justify-center '>Date</p> 
+            <p className='flex justify-center'>Remove</p>
+        </div>
+
+        <div className="my-5 py-5  bg-gray-50 overflow-y-scroll h-2/3">
+
+        
+
+
+            {sliderImageData.map((product,i) => {
+                
+                return  <div key={i} className="   items-center border-b-2  grid grid-cols-5 gap-1 md:gap-2 lg:gap-10 py-1 
+                border-gray-400 ">
+                            <img src={product.image} width={80} height={80} alt={product.title}  className=" mx-auto" />
+                            
+                            <p className='flex justify-center '>{product.title}</p> 
+                            <p className='flex justify -center'>{product.date}</p>
+                            <img className="cursor-pointer  mx-auto " width={10} height={10} src={remove_icon} alt="" onClick={() => {remove_product(product.id)}} />
+                            
+
+                        </div>
+                
+            })}
+        </div>
+
+        
+
+    </div>
+  )
+}
+export default SliderImageList
---
+++ admin/src/Components/SliderImagesAdd.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react"; 
+import upload_area from '../assets/upload_area.svg';
+ 
+
+const SliderImagesAdd = () => {
+    const [image, setImage] = useState(null); 
+    const [sliderImageData, setSliderImageData] = useState({
+      title: "", 
+      date: "",
+    });
+  
+    const imageHandler = (e) => {
+      setImage(e.target.files[0]);
+    };
+  
+   
+    const changeHandler = (e) => {
+      setSliderImageData({ ...sliderImageData, [e.target.name]: e.target.value });
+    };
+  
+    const Add_Product = async () => {
+      // Upload cover image
+      let responseData;
+      let formData = new FormData();
+      formData.append('image', image);
+  
+      await fetch('http://localhost:4000/upload', {
+        method: 'POST',
+        body: formData,
+      }).then(res => res.json()).then((data) => {
+        responseData = data;
+      });
+  
+    
+  
+      // Save product details in the database
+      if (responseData.success  ) {
+        let product = {
+          ...sliderImageData,
+          image: responseData.image_url, 
+        };
+  
+        await fetch('http://localhost:4000/sliderimageadd', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(product),
+        }).then(res => res.json()).then((data) => {
+          if(data.success) {
+            alert('Slider Image Added');
+            setSliderImageData({
+              title: "", 
+              date: "",
+            });  
+            setImage(null); 
+          } else {
+              alert('Slider image not added');
+          }  
+        });
+      }
+    };
+  
+    return (
+      <div>
+        <div className='text-4xl flex items-center justify-center py-10'> Slider image Upload</div>
+        <div className="flex flex-col gap-7 p-16 bg-gray-200 ml-5 my-5 w-auto sm:w-[500px] lg:w-[750px] xl:w-[1280px] font-custom-robot">
+          <div className="space-y-2">
+            <p>Title</p>
+            <input
+              value={sliderImageData.title}
+              onChange={changeHandler}
+              className="text-sm p-2 w-[100%]"
+              type="text"
+              name="title"
+              placeholder="Type here"
+            />
+          </div>
+   
+          <div className="space-y-2">
+            <p>Date</p>
+            <input
+              value={sliderImageData.date}
+              onChange={changeHandler}
+              className="text-sm p-2 w-[100%]"
+              type="date"
+              name="date"
+              placeholder="Type here"
+            />
+          </div>
+  
+          <div className="space-y-2">
+            <p>Slider Image</p>
+            <div className="w-[80px] md:w-[150px]">
+              <label htmlFor="file-input">
+                <img
+                  className="cursor-pointer"
+                  src={image ? URL.createObjectURL(image) : upload_area}
+                  alt=""
+                />
+                <input
+                  onChange={imageHandler}
+                  type="file"
+                  name="image"
+                  id="file-input"
+                  hidden
+                />
+              </label>
+            </div>
+          </div>
+  
+          
+  
+          <button
+            onClick={Add_Product}
+            className="w-60 bg-red-400 p-4 rounded-3xl border-2 border-gray-800 my-5 font-semibold hover:text-gray-300 hover:bg-red-500 transition-colors duration-100"
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    );
+  };
+  
+
+export default SliderImagesAdd
