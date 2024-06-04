f6160ef96722c8899334a1e3359e460a0d47e98e
Weerawi <weerawikramatharindu@gmail.com>
2024-06-04 23:04:44 +0530

    context api upadate

--- slqs-eng/src/Context/SlqsContext.jsx
+++ slqs-eng/src/Context/SlqsContext.jsx
@@ -1,17 +1,56 @@
-import React, { createContext } from 'react'
-import csr_data from '../Components/Assets/csr_data'
-import gallery_data from '../Components/Assets/gallery_data'
+import React, { createContext, useEffect, useState } from 'react'
+// import csr_data from '../Components/Assets/csr_data'
+// import gallery_data from '../Components/Assets/gallery_data'
 import article_data from '../Components/Assets/article_data'
 
 export const SlqsContext = createContext(null);
 
 const SlqsContextProvider = (props) => {
 
+  const [galallproducts,setGalAllProducts] = useState([]);
+  const [csrallproducts,setCsrAllProducts] = useState([]);
+  const [articlealldata,setArticleAllData] = useState([]);
+  const [allsliderimages,setAllSliderImages] = useState([]);
+
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/csrallproducts')
+    .then((res)=>res.json())
+    .then((data)=>setCsrAllProducts(data))
+ 
+  },[])
+
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/galallproducts')
+    .then((res)=>res.json())
+    .then((data)=>setGalAllProducts(data))
+ 
+  },[])
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/articlealldata')
+    .then((res)=>res.json())
+    .then((data)=>setArticleAllData(data))
+ 
+  },[]) 
+
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/allsliderimages')
+    .then((res)=>res.json())
+    .then((data)=>setAllSliderImages(data))
+ 
+  },[]) 
+
 
   const contextValue = {
-    csr_data,
-    gallery_data,
-    article_data
+    
+    article_data,
+    articlealldata,
+    galallproducts,
+    csrallproducts,
+    allsliderimages
   }
 
   return (
