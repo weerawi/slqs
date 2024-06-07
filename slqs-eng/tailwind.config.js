ce8b381ef936c0420941961712437c677090114b
Weerawi <weerawikramatharindu@gmail.com>
2024-06-07 14:15:56 +0530

    add new fonts and toatidy package

--- slqs-eng/tailwind.config.js
+++ slqs-eng/tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'custom-poppin': ['Custom Font', 'Poppins'],
+        'custom-kode': ['Custom Font', 'Kode Mono'],
+        'custom-robot': ['Custom Font', 'Roboto Mono'],
+      },
+    },
   },
   plugins: [],
 }
